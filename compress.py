e03f709a7db0b44ac69a97694ee74816810c3d26
mikebond <mikebond84@gmail.com>
2024-09-12 14:08:29 -0400

    added sample files for testing, started how hash will be generated and ordered

--- compress.py
+++ compress.py
@@ -1,8 +1,57 @@
-#! /usr/bin/python3
+#!/usr/bin/env python3
 
+import os
 import hashlib
-#from PIL import Image
-# windows files in wsl = /mnt/c/Users/
 
-#exfile="/mnt/c/Users/mikeb/Downloads/sample1.jpeg"
-#print(exfile)
+def transform_hash (direc):
+    for root,dirs,files in os.walk(direc):
+        for file in files:
+            print(os.path.join(root, file))
+'''
+def makehash(baseDir):
+    for entry in os.scandir(baseDir):
+        if entry.is_file():
+            yield os.path.join(baseDir, entry.name)
+        else:
+            yield scanRecurse(entry.path)
+  
+for i in makehash('./a_sample_dir'):
+    print(i)
+#from PIL import Image
+'''
+'''
+goal is make a tar script that will first hash
+image/doc add that hash to a db, compress with xz
+either on file or folder level to send to a cloud 
+backup 
+'''
+
+# first take hashes of a dir of files and print
+# https://builtin.com/data-science/python-list-files-in-directory
+
+#def dir_to_list():
+
+#files = os.listdir("./sample_dir/")
+
+#a=os.scandir()
+'''
+    for x in range(len(l)):
+        print (a[x])
+'''
+'''
+#md5 - hashlib.md5()
+
+#with open(sys.argv[1], 'rb') as f:
+ #   while True:
+# BUF_SIZE = 65536 
+# reads in 64kb chunks if ram is limited
+
+
+# md5 is being used for speed and security is unecessary
+# for the purpose it is serving
+
+def sha256sum(filename)
+    with open(filename, )
+
+print(exfile)
+'''
--- wiki.md
+++ wiki.md
@@ -1,1 +1,9 @@
+troubleshooting steps
+
 python -m venv /path/to/new/virtual/environment
+
+python -m venv .venv
+
+ctrl+shift+p open command pane
+enter python restart lang
+execute
