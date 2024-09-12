8f7a9f9efd5a9f3bb279f3e4aa75b44ba2159890
mikebond <mikebond84@gmail.com>
2024-09-12 10:39:56 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CompressoSync
 A utility to automatically archive, compress, sync and diff documents and images from local storage
+
---
+++ compress.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+
+import hashlib
+#from PIL import Image
+# windows files in wsl = /mnt/c/Users/
+
+#exfile="/mnt/c/Users/mikeb/Downloads/sample1.jpeg"
+#print(exfile)
