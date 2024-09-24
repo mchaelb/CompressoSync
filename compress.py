bdc561cce2a06fb2b9d9deb2eb9c7d19aff19794
mikebond <mikebond84@gmail.com>
2024-09-24 14:52:16 -0400

    created a dictionary "collection_dict" with file(path/to/name: file_hash

--- compress.py
+++ compress.py
@@ -8,21 +8,21 @@ import hashlib
 
 '''
 glob function grabs a list of files within a directory based on 
-a regex pattern of directories/subdirectories. i.e. is not directory
-traversal per se, but pattern matching. therefore, glob is best for
-known file paths/directories, where as os.walk would be better for
-unknown recursive directories, finding each subdir in a dir tree.
-os.walk may be better for tracking absolute paths in a more complex 
-and dynamic filesystem both for archiving (backups will be complete),
-and a visual reminder of actual dir/subdir/filename organization.
-for speed, os.walk is not ideal for the end state of this application,
-which is to efficiently compress, diff, and then push (if needed) backups.
-note that glob only takes 1 positional argument, so file naming convention
-will have to fit into pattern established by /path/to/dir/**/* python 3.5
+a regex pattern of directories/subdirectories, via pattern matching. 
+therefore, glob is best for known file paths/directories, where 
+os.walk would be better for unknown recursive directory paths, 
+finding each subdir in a dir tree. os.walk is better for tracking 
+paths in a more complex and dynamic filesystem both for archiving
+and documentation for dir/subdir/filename organization. os.walk is not 
+ideal for the end state of this application, which is to efficiently 
+compress, diff, and then push (if needed) backups. note that glob only 
+takes 1 positional argument, so file naming convention will have to 
+fit into pattern established by /path/to/dir/**/* python 3.5
 supports **/*, which is a recursive wildcard pattern. 
 '''
 
 # FIRST option glob alone to get a list of files
+
 # for file in sorted(glob.glob('./*_sample_dir/**')):
 #     print(file)
 
@@ -33,16 +33,25 @@ either on file or folder level to send to a cloud
 backup 
 '''
 
-# SECOND option to list files and hash them
+# SECOND option to list files, hash, and add to dictionary
+# collection_dict = {}
+
 # for file in sorted(glob.glob('./*_sample_dir/**')):
 #     with open(file, 'rb') as f:
 #         file_hash = hashlib.md5(f.read()).hexdigest()
-#     print(f"File: {file}, MD5 Hash: {file_hash}")
+#     # this adds file path and hash to dictionary object
+#     collection_dict[file,file_hash] = file_hash
+#     # this prints filedirectory_hash in a string to verify
+#     # print(f"{file}:{file_hash}") 
 
-# BUF_SIZE = 65536 
-# reads in 64kb chunks if ram is limited
 
-BUF_SIZE = 65536  # reads in 64kb chunks
+
+# THIRD option reads in 64kb chunks if ram is limited
+# NOTE: md5 is being used for speed and security is unecessary
+# for the purpose it is serving
+
+collection_dict = {}
+BUF_SIZE = 65536
 
 for file in sorted(glob.glob('./*_sample_dir/**')):
     md5 = hashlib.md5()
@@ -53,7 +62,7 @@ for file in sorted(glob.glob('./*_sample_dir/**')):
                 break
             md5.update(data)
     file_hash = md5.hexdigest()
-    print(f"File: {file}, MD5 Hash: {file_hash}")
-
-# md5 is being used for speed and security is unecessary
-# for the purpose it is serving
+    # Add the file path as the key and the MD5 hash as the value
+    collection_dict.update({file: file_hash})
+    # this adds file path and hash to dictionary object
+print(collection_dict)
